5aa0b1fdea739af162a6a3a61981519970556214
Natalie Rojas <natalie.rojas113@gmail.com>
2021-08-29 11:02:25 -0400

    Added routes & components

--- client/routes.js
+++ client/routes.js
@@ -1,66 +1,115 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React, {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
-import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {get, isEqual} from 'lodash'
+import {
+  Login,
+  Home,
+  Class,
+  ClassPrograms,
+  Daycare,
+  Month,
+  OurFocus,
+  Staff,
+  Week
+} from './components'
 import {me} from './store'
 
 /**
  * COMPONENT
  */
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData()
-  }
+const Routes = () => {
+  const userData = useSelector(state => get(state, 'user', {}), isEqual)
+  const isLoggedIn = !!userData.id
 
-  render() {
-    const {isLoggedIn} = this.props
+  const dispatch = useDispatch()
 
-    return (
-      <Switch>
-        {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
-          </Switch>
-        )}
-        {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
-      </Switch>
-    )
-  }
+  console.log(isLoggedIn)
+
+  // const titleParts = useSelector(state => get(state, ["formState", "field_title", 'values', 0, 'parts'], []), isEqual);
+
+  useEffect(() => {
+    dispatch(me())
+  }, [])
+
+  return (
+    <Switch>
+      {/* Routes placed here are available to all visitors */}
+      <Route path="/login" component={Login} />
+      <Route path="/" component={Home} />
+      <Route path="/focus" component={OurFocus} />
+      <Route path="/classes" component={ClassPrograms} />
+      <Route path="/classes/:id" component={Class} />
+      <Route path="/daycare" component={Daycare} />
+      <Route path="/staff" component={Staff} />
+      <Route path="/month" component={Month} />
+      {/* Below is for current month event (user change the content every month*/}
+      <Route path="/month/:weekId" component={Week} />
+
+      {/* We are not sure if we need this....
+        <Route path="/newsletter" component={NewsLetter} /> */}
+    </Switch>
+  )
 }
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
+export default withRouter(Routes)
 
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
+// /**
+//  * COMPONENT
+//  */
+// class Routes extends Component {
+//   componentDidMount() {
+//     this.props.loadInitialData()
+//   }
 
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+//   render() {
+//     const {isLoggedIn} = this.props
 
-/**
- * PROP TYPES
- */
-Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+//     return (
+//       <Switch>
+//         {/* Routes placed here are available to all visitors */}
+//         <Route path="/login" component={Login} />
+//         <Route path="/signup" component={Signup} />
+//         {isLoggedIn && (
+//           <Switch>
+//             {/* Routes placed here are only available after logging in */}
+//             <Route path="/home" component={UserHome} />
+//           </Switch>
+//         )}
+//         {/* Displays our Login component as a fallback */}
+//         <Route component={Login} />
+//       </Switch>
+//     )
+//   }
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+//     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+//     isLoggedIn: !!state.user.id
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     loadInitialData() {
+//       dispatch(me())
+//     }
+//   }
+// }
+
+// // The `withRouter` wrapper makes sure that updates are not blocked
+// // when the url changes
+// export default withRouter(connect(mapState, mapDispatch)(Routes))
+
+// /**
+//  * PROP TYPES
+//  */
+// Routes.propTypes = {
+//   loadInitialData: PropTypes.func.isRequired,
+//   isLoggedIn: PropTypes.bool.isRequired
+// }
